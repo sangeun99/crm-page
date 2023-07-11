ecd1c1246cbbca4f2fafd66215062b57e96d27e8
sangeun <sangeun99@khu.ac.kr>
2023-07-11 11:20:13 +0900

    ⚰️ remove: 불필요한 test 코드와 csv 파일 삭제

--- app.py
+++ app.py
@@ -1,30 +1,19 @@
 from flask import Flask, render_template
 
-from test import User
-
 from view.user_view import user_bp
 from view.order_view import order_bp
 from view.orderitem_view import orderitem_bp
 from view.item_view import item_bp
 from view.store_view import store_bp
-from view.common import get_results
+
 
 app = Flask(__name__, static_folder="static")
-
 
 app.register_blueprint(user_bp)
 app.register_blueprint(order_bp)
 app.register_blueprint(orderitem_bp)
 app.register_blueprint(item_bp)
 app.register_blueprint(store_bp)
-
-
-@app.route('/test')
-def test():
-    result = get_results("SELECT * FROM users WHERE name = ?", "김아린")
-
-    print(result)
-    return "complete1"
 
 @app.route("/")
 def root():
