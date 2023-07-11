090fbcda8728982e0f6042f8cd4e47054de42c84
sangeun <sangeun99@khu.ac.kr>
2023-07-11 11:23:22 +0900

    🐛 fix: sqlite uuid 인식 불가능 문제 해결

--- models/id.py
+++ models/id.py
@@ -2,7 +2,8 @@ import uuid
 
 class Id():
     def __init__(self):
-        self.id = uuid.uuid4()
+        # sqlite3 uuid type 지원하지 않아 string으로 변환
+        self.id = str(uuid.uuid4())
     
     def generate(self):
         return self.id
