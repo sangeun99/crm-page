04c9bfc87c0d16634f846f49cbcda04ffaf19650
sangeun <sangeun99@khu.ac.kr>
2023-07-28 16:48:30 +0900

    🎉 init: (v2) orm 이용한 crm 페이지 구성

--- models/id.py
+++
@@ -1,9 +0,0 @@
-import uuid
-
-class Id():
-    def __init__(self):
-        # sqlite3 uuid type 지원하지 않아 string으로 변환
-        self.id = str(uuid.uuid4())
-    
-    def generate(self):
-        return self.id
